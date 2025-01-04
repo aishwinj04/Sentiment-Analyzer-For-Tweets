e0fa26d081bce1ef3450803514fa37ebb9cef0d4
aishwinj04 <aishwin@gmail.com>
2025-01-04 00:47:48 -0500

    Update main.py
    Conditional threshold

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 
 text1 = 'Hey, what a beautiful day! How amazing it is!'
-analyze = analyzer.polarity_scores(text1) # positive/negative coef 
+analyze = analyzer.polarity_scores(text1) # get coef
 print(analyze)
+print(type(analyze))
+
+# access compound key of dict
+compound = analyze['compound']
+print(compound)
+
+if compound > 0:
+    print('Positive')
+else:
+    print('Negative')
