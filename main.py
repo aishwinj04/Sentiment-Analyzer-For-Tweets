51cf92cd29e0792d560ef29fc2b44012e4138779
aishwinj04 <aishwin@gmail.com>
2025-01-04 00:59:51 -0500

    Find matching tweets

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import errno
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-x = nltk.corpus.twitter_samples.strings()[1]
-print(x)
+def get_tweet(term):
 
-analyzer = SentimentIntensityAnalyzer()
+    matched_tweets = []
+    tweets = nltk.corpus.twitter_samples.strings()
+    for tweet in tweets:
+        if term.lower() in tweet.lower():
+            matched_tweets.append(tweet)
 
-text1 = 'Hey, what a beautiful day! How amazing it is!'
-analyze = analyzer.polarity_scores(x) # get coef
-print(analyze)
-print(type(analyze))
+    return matched_tweets
 
-# access compound key of dict
-compound = analyze['compound']
-print(compound)
 
-# output message
-if compound > 0:
-    print('Positive')
-else:
-    print('Negative')
+tweets_list = get_tweet('apple')
+print(tweets_list)
+
+
+
+# analyzer = SentimentIntensityAnalyzer()
+
+# text1 = 'Hey, what a beautiful day! How amazing it is!'
+# analyze = analyzer.polarity_scores(text1) # get coef
+# print(analyze)
+# print(type(analyze))
+
+# # access compound key of dict
+# compound = analyze['compound']
+# print(compound)
+
+# # output message
+# if compound > 0:
+#     print('Positive')
+# else:
+#     print('Negative')
 
