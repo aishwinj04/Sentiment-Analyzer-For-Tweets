33972018a446574eecab6cb656df83cf18404532
aishwinj04 <aishwin@gmail.com>
2025-01-04 00:53:36 -0500

    Use twitter tweets

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import errno
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-#nltk.download(vader_lexicon)
+x = nltk.corpus.twitter_samples.strings()[1]
+print(x)
 
 analyzer = SentimentIntensityAnalyzer()
 
 text1 = 'Hey, what a beautiful day! How amazing it is!'
-analyze = analyzer.polarity_scores(text1) # get coef
+analyze = analyzer.polarity_scores(x) # get coef
 print(analyze)
 print(type(analyze))
 
@@ -14,7 +16,9 @@ print(type(analyze))
 compound = analyze['compound']
 print(compound)
 
+# output message
 if compound > 0:
     print('Positive')
 else:
     print('Negative')
+
